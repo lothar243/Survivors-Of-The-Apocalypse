bc32a8377e832eb9ed2fbc24028828a458f9c4f8
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-02-25 23:45:15 -0700

    added card stub

---
+++ app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/PlayCard.java
@@ -0,0 +1,22 @@
+package edu.umt.csdept.survivorsoftheapocalypse;
+
+/**
+ * Created by sinless on 2/25/16.
+ */
+public class PlayCard implements Card {
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void onAquire(GameState gameState) {
+
+    }
+
+    @Override
+    public void onPlay(GameState gameState) {
+
+    }
+}
