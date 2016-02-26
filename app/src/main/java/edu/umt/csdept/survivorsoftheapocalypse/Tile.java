9c9cf0b4be7d9c8a54ca6c5379e5c655de2e1d51
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-02-25 23:50:39 -0700

    flesh out tile

--- app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Tile.java
+++ app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Tile.java
@@ -1,9 +1,15 @@
 package edu.umt.csdept.survivorsoftheapocalypse;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by sinless on 2/25/16.
  */
 public class Tile implements Card{
+    private Bitmap bitmap;
+    private String resource;
+    private int banditDanger;
+    private int zombieDanger;
 
     @Override
     public void draw() {
@@ -18,5 +24,37 @@ public class Tile implements Card{
     @Override
     public void onPlay(GameState gameState) {
     //do what needs to be done to add tile to board
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public int getBanditDanger() {
+        return banditDanger;
+    }
+
+    public void setBanditDanger(int banditDanger) {
+        this.banditDanger = banditDanger;
+    }
+
+    public int getZombieDanger() {
+        return zombieDanger;
+    }
+
+    public void setZombieDanger(int zombieDanger) {
+        this.zombieDanger = zombieDanger;
     }
 }
