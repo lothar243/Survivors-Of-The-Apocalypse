f09004746cf2cb8dc49fafc04604dca1947c60c7
jeff <jeffarends@gmail.com>
2016-02-25 21:48:59 -0700

    Fixed some errors

--- app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/GameState.java
+++ app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/GameState.java
@@ -1,9 +1,11 @@
 package edu.umt.csdept.survivorsoftheapocalypse;
 
+import java.util.ArrayList;
+
 /**
  * Created by sinless on 2/11/16.
  */
-public class Location{
+class Location{
     int xlocation;
     int ylocation;
 }
@@ -15,11 +17,11 @@ public class GameState {
     public GameState(int boardX, int boardY) {
         String[][] tileNames = new String[boardX][boardY];
         int[][] tileResources = new int[boardX][boardY];
-        ArrayList<Location> player1Positions = new ArrayList<Location>;
-        ArrayList<Location> player2Positions = new ArrayList<Location>;
-        ArrayList<Location> wallLocations = newArrayList<Location>;
-        Deck tileDeck = new Deck()
-        Deck playerDeck = new Deck()
+        ArrayList<Location> player1Positions = new ArrayList<Location>();
+        ArrayList<Location> player2Positions = new ArrayList<Location>();
+        ArrayList<Location> wallLocations = new ArrayList<Location>();
+        Deck tileDeck = new Deck();
+        Deck playerDeck = new Deck();
 
 
     }
