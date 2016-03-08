e0b6994b8a039e91351a1359ed12b4b5757c357e
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-03-07 22:32:36 -0700

    more playerStuff

--- app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Player.java
+++ app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Player.java
@@ -14,4 +14,36 @@ public class Player {
     public Player(String playerName) {
         this.playerName = playerName;
     }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public ArrayList<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
+
+    public int getWoodCount() {
+        return woodCount;
+    }
+
+    public void setWoodCount(int woodCount) {
+        this.woodCount = woodCount;
+    }
+
+    public int getFoodCount() {
+        return foodCount;
+    }
+
+    public void setFoodCount(int foodCount) {
+        this.foodCount = foodCount;
+    }
 }
