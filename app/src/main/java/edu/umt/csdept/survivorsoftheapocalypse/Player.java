b26eebf8ca0234efb98c712a6debfe9746bdb4fd
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-03-07 18:23:02 -0700

    added player

---
+++ app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Player.java
@@ -0,0 +1,14 @@
+package edu.umt.csdept.survivorsoftheapocalypse;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sinless on 3/7/16.
+ */
+public class Player {
+    ArrayList<Location> locations;
+    int woodCount;
+    int foodCount;
+
+
+}
