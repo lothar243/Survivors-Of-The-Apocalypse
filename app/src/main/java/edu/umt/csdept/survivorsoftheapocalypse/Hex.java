2f070365054db2125c22064fb8e238937ae50eac
jeff <jeffarends@gmail.com>
2016-03-28 18:03:33 -0600

    Altered the gameboard to show a colored meeple and the resource count

--- app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Hex.java
+++ app/src/main/java/edu/umt/csdept/survivorsoftheapocalypse/Hex.java
@@ -29,6 +29,9 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.util.Log;
@@ -62,40 +65,46 @@ public class Hex {
     static final int hexWidth = Math.round(2f * sideLength + horizontalGap) + horizontalGap;
     static final int hexHeight = Math.round((float)Math.sqrt(3)* sideLength + verticalGap) + verticalGap;
 
+    static final int maxImageWidth = 150, maxImageHeight = 150, imageX = 100, imageY = 10;
+    static final int textX = 260, textY = 200;
+    static final int maxMeepleWidth = 150, maxMeepleHeight = 150, meepleX = 100, meepleY = 180;
+
+    static Resources resources;
 
     int rCoord, gCoord;
     private Bitmap image;
+    private Bitmap meepleBitmap = null;
 
+    int numResources;
+    int resourceTypeColor;
 
-
-    public Hex(int rCoord, int gCoord, Resources resources) {
+    public Hex(int rCoord, int gCoord, Resources _resources, int resourceID, int numResources, int resourceTypeColor) {
         this.rCoord = rCoord;
         this.gCoord = gCoord;
-        image = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, R.drawable.hex),
-                hexWidth - horizontalGap, hexHeight - verticalGap, false);
+        Bitmap hex = BitmapFactory.decodeResource(_resources, R.drawable.hex);
+        this.image = Bitmap.createBitmap(hexWidth - horizontalGap, hexHeight - verticalGap, hex.getConfig());
+        Canvas canvas = new Canvas(this.image);
+        if(resources == null) {
+            resources = _resources;
+        }
 
+        Matrix hexMatrix = new Matrix();
+        float[] fromVector = {0f, 0f, hex.getWidth(), hex.getHeight()};
+        float[] toVector = {0f, 0f, image.getWidth(), image.getHeight()};
+        hexMatrix.setPolyToPoly(fromVector, 0, toVector, 0, 2);
+        canvas.drawBitmap(hex, hexMatrix, null);
 
-    }
-    public Hex(int rCoord, int gCoord, Resources resources, int resourceID) {
-        this.rCoord = rCoord;
-        this.gCoord = gCoord;
-        this.image = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, resourceID),
-                hexWidth - horizontalGap, hexHeight - verticalGap, false);
-    }
-    public Hex(int rCoord, int gCoord, Bitmap image) {
-        this.rCoord = rCoord;
-        this.gCoord = gCoord;
-        this.image = Bitmap.createScaledBitmap(image, hexWidth - horizontalGap, hexHeight - verticalGap, false);
-    }
+        Matrix imageMatrix = new Matrix();
+        Bitmap picture = BitmapFactory.decodeResource(_resources, resourceID);
+        float scale = Math.min((float)maxImageWidth / picture.getWidth(), (float)maxImageHeight / picture.getHeight());
+        imageMatrix.setScale(scale, scale);
+        imageMatrix.postTranslate(imageX, imageY);
+        canvas.drawBitmap(picture, imageMatrix, null);
 
-    public void changeImage(Resources resources, int resourceID) {
-        this.image = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, resourceID),
-                hexWidth - horizontalGap, hexHeight - verticalGap, false);
-    }
+        this.numResources = numResources;
+        this.resourceTypeColor = resourceTypeColor;
 
-    public void changeImage(Bitmap image) {
-        this.image = Bitmap.createScaledBitmap(image,
-                hexWidth - horizontalGap, hexHeight - verticalGap, false);
+        setMeeple(rCoord);
     }
 
     public PointF getCenter() {
@@ -105,10 +114,47 @@ public class Hex {
     public void draw(Canvas canvas) {
         if(image == null) return;
         PointF center = getCenter();
-        canvas.drawBitmap(image, center.x - .5f * hexWidth, center.y - .5f * hexHeight, null);
+        float offsetX = center.x - .5f * hexWidth, offsetY = center.y - .5f * hexHeight;
+        canvas.drawBitmap(image, offsetX, offsetY, null);
+        Paint paint = new Paint();
+        paint.setColor(resourceTypeColor);
+        paint.setTextSize(150);
+        canvas.drawText(numResources + "", offsetX + textX, offsetY + textY, paint);
+        if(meepleBitmap != null) canvas.drawBitmap(meepleBitmap, offsetX + meepleX, offsetY + meepleY, null);
     }
     public Location getLocation() {
         return new Location(rCoord, gCoord);
+    }
+
+    public void setMeeple(int playerNumber) {
+        int meepleResourceID;
+        switch (playerNumber) {
+            case 0:
+                meepleResourceID = R.drawable.p1meeple;
+                break;
+            case 1:
+                meepleResourceID = R.drawable.p2meeple;
+                break;
+            case 2:
+                meepleResourceID = R.drawable.p3meeple;
+                break;
+            case 3:
+                meepleResourceID = R.drawable.p4meeple;
+                break;
+            case 4:
+                meepleResourceID = R.drawable.p5meeple;
+                break;
+            case 5:
+                meepleResourceID = R.drawable.p6meeple;
+                break;
+            default:
+                meepleResourceID = R.drawable.meeple;
+        }
+        meepleBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, meepleResourceID),
+                maxImageWidth, maxMeepleHeight, false);
+        if(playerNumber == -1) {
+            meepleBitmap = null;
+        }
     }
 
     public static PointF rectCoordsToHex(float x, float y) {
@@ -129,7 +175,6 @@ public class Hex {
     }
 
     public static Point getHexIndex(float x, float y) {
-
         // determine the hex whose center is to the bottom left
         PointF hexCoords = rectCoordsToHex(x, y);
         int referenceR = (int)hexCoords.x;
